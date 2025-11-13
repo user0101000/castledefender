841544811ea22cab2e862c83e8f18c0a67ef1489
user0101000 <blancaribeirosilva72@gmail.com>
2025-11-13 14:18:44 -0300

    Initial commit: Unity project setup

---
+++ Assets/VidaInimigo.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+public class VidaInimigo : MonoBehaviour
+{
+    public int hp;
+    
+    public void ReceberDano(int dano)
+    {
+        hp -= dano;
+        if (hp <= 0)
+        {
+            Morrer();
+        }
+    }
+    
+    private void Morrer()
+    {
+        // Aqui pode colocar animação, som, etc
+        Destroy(gameObject);
+    }
+}
